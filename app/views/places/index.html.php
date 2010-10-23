08076a6256b5719c45174249bb55a6573e4a5917
jairhenrique <jair.henrique@gmail.com>
2010-10-23 01:17:57 -0200

    Correção do link

--- app/views/places/index.html.php
+++ app/views/places/index.html.php
@@ -1,7 +1,9 @@
 <?php if ($search and !isset ($_GET['checkin'])): ?>
 	<h3>
 		Locais próximos ao CEP <?=$zipcode; ?>
-		<a href="/?checkin">(mudar onde estou)</a>
+		<?php
+			echo $this->html->link("(mudar onde estou)","/?checkin");
+		?>
 	</h3>
 
 	<ul>
@@ -15,9 +17,10 @@
 	<h3>Onde você está?</h3>
 	<span>
 	<form action="" method="GET" style="width: 115px;">
-	CEP: <input type="text" name="cep" value="<?=$zipcode; ?>" style="width: 100px;">
-    <input type="submit" value="Estou aqui">
-</form>
+		<label for="cep">CEP:</label>
+		<input type="text" id="cep" name="cep" value="<?=$zipcode; ?>" style="width: 100px;">
+		<input type="submit" value="Estou aqui">
+		</form>
 	</span>
 
 <?php endif; ?>
