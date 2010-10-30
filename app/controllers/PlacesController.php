fdbcad88381927b12852586f8741be3fd7182d39
Eher <alexandre@skd.com.br>
2010-10-30 01:17:50 +0000

    Aplicação da nova busca recursiva a todas as buscas. Refatoração de algumas coisas que faltaram.

--- app/controllers/PlacesController.php
+++ app/controllers/PlacesController.php
@@ -7,276 +7,269 @@ use lithium\storage;
 
 class PlacesController extends \lithium\action\Controller {
 
-    public function index() {
-        $placeId = \lithium\storage\Session::read('placeId');
-        $placeName = \lithium\storage\Session::read('placeName');
-        $zipcode = \lithium\storage\Session::read('zipcode');
-        $cityState = \lithium\storage\Session::read('cityState');
-        $lat = \lithium\storage\Session::read('lat');
-        $lng = \lithium\storage\Session::read('lng');
+	public function index() {
+		$placeId = \lithium\storage\Session::read('placeId');
+		$placeName = \lithium\storage\Session::read('placeName');
+		$zipcode = \lithium\storage\Session::read('zipcode');
+		$cityState = \lithium\storage\Session::read('cityState');
+		$lat = \lithium\storage\Session::read('lat');
+		$lng = \lithium\storage\Session::read('lng');
 
-        if (empty($placeId) && empty($placeName) && empty($zipcode) && empty($cityState) && (empty($lat) or empty($lng))) {
-            $geo = new \app\models\Geocode();
-            $geo->getByIp();
-            $lat = $geo->getLatitude();
-            $lng = $geo->getLongitude();
+		if (empty($placeId) && empty($placeName) && empty($zipcode) && empty($cityState) && (empty($lat) or empty($lng))) {
+			$geo = new \app\models\Geocode();
+			$geo->getByIp();
+			$lat = $geo->getLatitude();
+			$lng = $geo->getLongitude();
 
-            if (empty($lat) || empty($lng)) {
-                $this->redirect('/places/checkin');
-            } else {
-                $this->redirect('/places/checkin?lat=' . $lat . '&lng=' . $lng);
-            }
-        }
+			if (empty($lat) || empty($lng)) {
+				$this->redirect('/places/checkin');
+			} else {
+				$this->redirect('/places/checkin?lat=' . $lat . '&lng=' . $lng);
+			}
+		}
 
-        return compact('placeId', 'placeName', 'zipcode', 'cityState', 'lat', 'lng');
-    }
+		return compact('placeId', 'placeName', 'zipcode', 'cityState', 'lat', 'lng');
+	}
 
-    public function search() {
-        $api = new \app\models\ApontadorApi();
+	public function search() {
+		$api = new \app\models\ApontadorApi();
 
-        $placeId = \lithium\storage\Session::read('placeId');
-        $placeName = \lithium\storage\Session::read('placeName');
-        $zipcode = \lithium\storage\Session::read('zipcode');
-        $cityState = \lithium\storage\Session::read('cityState');
-        $lat = \lithium\storage\Session::read('lat');
-        $lng = \lithium\storage\Session::read('lng');
+		$placeId = \lithium\storage\Session::read('placeId');
+		$placeName = \lithium\storage\Session::read('placeName');
+		$zipcode = \lithium\storage\Session::read('zipcode');
+		$cityState = \lithium\storage\Session::read('cityState');
+		$lat = \lithium\storage\Session::read('lat');
+		$lng = \lithium\storage\Session::read('lng');
 
-        $searchName = empty($_GET['name']) ? '' : $_GET['name'];
+		$searchName = '';
 
-        if (!empty($placeId)) {
-            $place = $api->getPlace(array('placeid' => $placeId));
-            $lat = $place->place->point->lat;
-            $lng = $place->place->point->lng;
-            $searchJson = $api->searchRecursive(array(
-                        'term' => $searchName,
-                        'radius_mt' => 100,
-                        'lat' => $lat,
-                        'lng' => $lng
-                    ));
-            $clear = array('zipcode', 'cityState', 'lat', 'lng');
-        } elseif (!empty($zipcode)) {
-            $searchJson = $api->searchByZipcode(array(
-                        'term' => $searchName,
-                        'radius_mt' => 100000,
-                        'zipcode' => $zipcode
-                    ));
-            $clear = array('placeId', 'cityState', 'lat', 'lng');
-        } elseif (!empty($cityState) and strstr($cityState, ',')) {
-            list($city, $state) = \explode(',', $cityState);
-            $searchJson = $api->searchByAddress(array(
-                        'term' => $searchName,
-                        'radius_mt' => 100000,
-                        'city' => trim($city),
-                        'state' => trim($state),
-                        'country' => 'BR'
-                    ));
-            $clear = array('placeId', 'zipcode', 'lat', 'lng');
-        } elseif (!empty($lat) and !empty($lng)) {
-            $searchJson = $api->searchRecursive(array(
-                        'term' => $searchName,
-                        'radius_mt' => 100000,
-                        'lat' => $lat,
-                        'lng' => $lng
-                    ));
-            $clear = array('placeId', 'zipcode', 'cityState');
-        } else {
-            $this->redirect('/places/checkin');
-        }
+		if (isset($_GET['name'])) {
+			$searchName = $_GET['name'];
 
-        $search = json_decode($searchJson, false);
+			if (!empty($placeId)) {
+				$place = $api->getPlace(array('placeid' => $placeId));
+				$lat = $place->place->point->lat;
+				$lng = $place->place->point->lng;
+				$search = $api->searchRecursive(array(
+							'term' => $searchName,
+							'lat' => $lat,
+							'lng' => $lng
+								), 'searchByPoint');
+				$clear = array('zipcode', 'cityState', 'lat', 'lng');
+			} elseif (!empty($zipcode)) {
+				$search = $api->searchRecursive(array(
+							'term' => $searchName,
+							'zipcode' => $zipcode
+								), 'searchByZipcode');
+				$clear = array('placeId', 'cityState', 'lat', 'lng');
+			} elseif (!empty($cityState) and strstr($cityState, ',')) {
+				list($city, $state) = \explode(',', $cityState);
+				$search = $api->searchRecursive(array(
+							'term' => $searchName,
+							'city' => trim($city),
+							'state' => trim($state),
+							'country' => 'BR'
+								), 'searchByAddress');
+				$clear = array('placeId', 'zipcode', 'lat', 'lng');
+			} elseif (!empty($lat) and !empty($lng)) {
+				$search = $api->searchRecursive(array(
+							'term' => $searchName,
+							'lat' => $lat,
+							'lng' => $lng
+								), 'searchByPoint');
+				$clear = array('placeId', 'zipcode', 'cityState');
+			} else {
+				$this->redirect('/places/checkin');
+			}
+		}
+		return compact('search', 'searchName', 'placeId', 'placeName', 'zipcode', 'cityState', 'lat', 'lng');
+	}
 
-        return compact('search', 'searchName', 'placeId', 'placeName', 'zipcode', 'cityState', 'lat', 'lng');
-    }
+	public function near() {
+		$api = new \app\models\ApontadorApi();
 
-    public function near() {
-        $api = new \app\models\ApontadorApi();
+		$placeId = \lithium\storage\Session::read('placeId');
+		$placeName = \lithium\storage\Session::read('placeName');
+		$zipcode = \lithium\storage\Session::read('zipcode');
+		$cityState = \lithium\storage\Session::read('cityState');
+		$lat = \lithium\storage\Session::read('lat');
+		$lng = \lithium\storage\Session::read('lng');
 
-        $placeId = \lithium\storage\Session::read('placeId');
-        $placeName = \lithium\storage\Session::read('placeName');
-        $zipcode = \lithium\storage\Session::read('zipcode');
-        $cityState = \lithium\storage\Session::read('cityState');
-        $lat = \lithium\storage\Session::read('lat');
-        $lng = \lithium\storage\Session::read('lng');
+		if (!empty($placeId)) {
+			$place = $api->getPlace(array('placeid' => $placeId));
+			$lat = $place->place->point->lat;
+			$lng = $place->place->point->lng;
+			$search = $api->searchRecursive(array(
+						'lat' => $lat,
+						'lng' => $lng
+							), 'searchByPoint');
+			$clear = array('zipcode', 'cityState', 'lat', 'lng');
+		} elseif (!empty($zipcode)) {
+			$search = $api->searchRecursive(array(
+						'zipcode' => $zipcode
+							), 'searchByZipcode');
+			$clear = array('placeId', 'cityState', 'lat', 'lng');
+		} elseif (!empty($cityState) and strstr($cityState, ',')) {
+			list($city, $state) = \explode(',', $cityState);
+			$search = $api->searchRecursive(array(
+						'city' => trim($city),
+						'state' => trim($state),
+						'country' => 'BR'
+							), 'searchByAddress');
+			$clear = array('placeId', 'zipcode', 'lat', 'lng');
+		} elseif (!empty($lat) and !empty($lng)) {
+			$search = $api->searchRecursive(array(
+						'lat' => $lat,
+						'lng' => $lng
+							), 'searchByPoint');
+			$clear = array('placeId', 'zipcode', 'cityState');
+		} else {
+			$this->redirect('/places/checkin');
+		}
 
-        if (!empty($placeId)) {
-            $place = $api->getPlace(array('placeid' => $placeId));
-            $lat = $place->place->point->lat;
-            $lng = $place->place->point->lng;
-            $search = $api->searchRecursive(array(
-                        'lat' => $lat,
-                        'lng' => $lng
-                    ));
-            $clear = array('zipcode', 'cityState', 'lat', 'lng');
-        } elseif (!empty($zipcode)) {
-            $search = $api->searchByZipcode(array(
-                        'zipcode' => $zipcode
-                    ));
-            $clear = array('placeId', 'cityState', 'lat', 'lng');
-        } elseif (!empty($cityState) and strstr($cityState, ',')) {
-            list($city, $state) = \explode(',', $cityState);
-            $search = $api->searchByAddress(array(
-                        'city' => trim($city),
-                        'state' => trim($state),
-                        'country' => 'BR'
-                    ));
-            $clear = array('placeId', 'zipcode', 'lat', 'lng');
-        } elseif (!empty($lat) and !empty($lng)) {
-            $search = $api->searchRecursive(array(
-                        'lat' => $lat,
-                        'lng' => $lng
-                    ));
-            $clear = array('placeId', 'zipcode', 'cityState');
-        } else {
-            $this->redirect('/places/checkin');
-        }
+		return compact('search', 'placeId', 'placeName', 'zipcode', 'cityState', 'lat', 'lng');
+	}
 
-        return compact('search', 'placeId', 'placeName', 'zipcode', 'cityState', 'lat', 'lng');
-    }
+	public function categories() {
+		$api = new \app\models\ApontadorApi();
 
-    public function categories() {
-        $api = new \app\models\ApontadorApi();
+		$placeId = \lithium\storage\Session::read('placeId');
+		$placeName = \lithium\storage\Session::read('placeName');
+		$zipcode = \lithium\storage\Session::read('zipcode');
+		$cityState = \lithium\storage\Session::read('cityState');
+		$lat = \lithium\storage\Session::read('lat');
+		$lng = \lithium\storage\Session::read('lng');
 
-        $placeId = \lithium\storage\Session::read('placeId');
-        $placeName = \lithium\storage\Session::read('placeName');
-        $zipcode = \lithium\storage\Session::read('zipcode');
-        $cityState = \lithium\storage\Session::read('cityState');
-        $lat = \lithium\storage\Session::read('lat');
-        $lng = \lithium\storage\Session::read('lng');
+		if (isset($_GET['all'])) {
+			$categories = $api->getCategories();
+		} else {
+			$categories = $api->getCategoriesTop();
+		}
+		return compact('categories', 'placeId', 'placeName', 'zipcode', 'cityState', 'lat', 'lng');
+	}
 
-        if (isset($_GET['all'])) {
-            $categoriesJson = $api->getCategories();
-        } else {
-            $categoriesJson = $api->getCategoriesTop();
-        }
-        $categories = json_decode($categoriesJson, false);
-        return compact('categories', 'placeId', 'placeName', 'zipcode', 'cityState', 'lat', 'lng');
-    }
+	public function category($categoryId) {
+		if (empty($categoryId)) {
+			$this->redirect('/places/categories');
+		}
+		$api = new \app\models\ApontadorApi();
 
-    public function category($categoryId) {
-        if (empty($categoryId)) {
-            $this->redirect('/places/categories');
-        }
-        $api = new \app\models\ApontadorApi();
-
-        $placeId = \lithium\storage\Session::read('placeId');
-        $placeName = \lithium\storage\Session::read('placeName');
-        $zipcode = \lithium\storage\Session::read('zipcode');
-        $cityState = \lithium\storage\Session::read('cityState');
-        $lat = \lithium\storage\Session::read('lat');
-        $lng = \lithium\storage\Session::read('lng');
+		$placeId = \lithium\storage\Session::read('placeId');
+		$placeName = \lithium\storage\Session::read('placeName');
+		$zipcode = \lithium\storage\Session::read('zipcode');
+		$cityState = \lithium\storage\Session::read('cityState');
+		$lat = \lithium\storage\Session::read('lat');
+		$lng = \lithium\storage\Session::read('lng');
 
 
-        $categoryJson = $api->getSubcategories(array('categoryid' => $categoryId));
-        $category = json_decode($categoryJson, false);
+		$category = $api->getSubcategories(array('categoryid' => $categoryId));
 
-        $categoryName = $category->category->name;
+		$categoryName = $category->category->name;
 
-        if (!empty($placeId)) {
-            $placeJson = $api->getPlace(array('placeid' => $placeId));
-            $place = json_decode($placeJson, false);
-            $lat = $place->place->point->lat;
-            $lng = $place->place->point->lng;
-            $searchJson = $api->searchByPoint(array(
-                        'category_id' => $categoryId,
-                        'lat' => $lat,
-                        'lng' => $lng
-                    ));
-            $clear = array('zipcode', 'cityState', 'lat', 'lng');
-        } elseif (!empty($zipcode)) {
-            $searchJson = $api->searchByZipcode(array(
-                        'category_id' => $categoryId,
-                        'zipcode' => $zipcode
-                    ));
-            $clear = array('placeId', 'cityState', 'lat', 'lng');
-        } elseif (!empty($cityState) and strstr($cityState, ',')) {
-            list($city, $state) = \explode(',', $cityState);
-            $searchJson = $api->searchByAddress(array(
-                        'category_id' => $categoryId,
-                        'city' => trim($city),
-                        'state' => trim($state),
-                        'country' => 'BR'
-                    ));
-            $clear = array('placeId', 'zipcode', 'lat', 'lng');
-        } elseif (!empty($lat) and !empty($lng)) {
-            $searchJson = $api->searchByPoint(array(
-                        'category_id' => $categoryId,
-                        'lat' => $lat,
-                        'lng' => $lng
-                    ));
-            $clear = array('placeId', 'zipcode', 'cityState');
-        } else {
-            $this->redirect('/places/checkin');
-        }
+		if (!empty($placeId)) {
+			$place = $api->getPlace(array('placeid' => $placeId));
+			$lat = $place->place->point->lat;
+			$lng = $place->place->point->lng;
+			$search = $api->searchRecursive(array(
+						'category_id' => $categoryId,
+						'lat' => $lat,
+						'lng' => $lng
+							), 'searchByPoint');
+			$clear = array('zipcode', 'cityState', 'lat', 'lng');
+		} elseif (!empty($zipcode)) {
+			$search = $api->searchRecursive(array(
+						'category_id' => $categoryId,
+						'zipcode' => $zipcode
+							), 'searchByZipcode');
+			$clear = array('placeId', 'cityState', 'lat', 'lng');
+		} elseif (!empty($cityState) and strstr($cityState, ',')) {
+			list($city, $state) = \explode(',', $cityState);
+			$search = $api->searchRecursive(array(
+						'category_id' => $categoryId,
+						'city' => trim($city),
+						'state' => trim($state),
+						'country' => 'BR'
+							), 'searchByAddress');
+			$clear = array('placeId', 'zipcode', 'lat', 'lng');
+		} elseif (!empty($lat) and !empty($lng)) {
+			$search = $api->searchRecursive(array(
+						'category_id' => $categoryId,
+						'lat' => $lat,
+						'lng' => $lng
+							), 'searchByPoint');
+			$clear = array('placeId', 'zipcode', 'cityState');
+		} else {
+			$this->redirect('/places/checkin');
+		}
 
-        $search = json_decode($searchJson, false);
+		return compact('search', 'categoryName', 'placeId', 'placeName', 'zipcode', 'cityState', 'lat', 'lng');
+	}
 
-        return compact('search', 'categoryName', 'placeId', 'placeName', 'zipcode', 'cityState', 'lat', 'lng');
-    }
+	public function checkin() {
+		$title = "Estou aqui";
+		$api = new \app\models\ApontadorApi();
 
-    public function checkin() {
-        $title = "Estou aqui";
-        $api = new \app\models\ApontadorApi();
-
-        if (!empty($_GET)) {
+		if (!empty($_GET)) {
 			if (!empty($_GET['placeId'])) {
 				$place = $api->getPlace(array('placeid' => $_GET['placeId']));
-                $placeName = $place->place->name;
-                $checkinData = array('placeId' => $_GET['placeId'], 'placeName' => $placeName);
+				$placeName = $place->place->name;
+				$checkinData = array('placeId' => $_GET['placeId'], 'placeName' => $placeName);
 			} elseif (!empty($_GET['lat']) and !empty($_GET['lng'])) {
-                $checkinData = array('lat' => $_GET['lat'], 'lng' => $_GET['lng']);
-            } elseif (!empty($_GET['cep'])) {
-                $checkinData = array('zipcode' => $_GET['cep']);
-            } elseif (!empty($_GET['cityState'])) {
-               	$checkinData = array('cityState' => $_GET['cityState']);
-            }
+				$checkinData = array('lat' => $_GET['lat'], 'lng' => $_GET['lng']);
+			} elseif (!empty($_GET['cep'])) {
+				$checkinData = array('zipcode' => $_GET['cep']);
+			} elseif (!empty($_GET['cityState'])) {
+				$checkinData = array('cityState' => $_GET['cityState']);
+			}
 
 			$this->doCheckin($checkinData);
 
-            $this->redirect('/');
-        }
-    }
+			$this->redirect('/');
+		}
+	}
 
 	private function doCheckin(Array $checkinData = array()) {
 		$checkinVars = array('zipcode', 'cityState', 'lat', 'lng', 'placeId');
 
 		foreach ($checkinVars as $method) {
-       		\lithium\storage\Session::write($method);
-        }
+			\lithium\storage\Session::write($method);
+		}
 
 		foreach ($checkinData as $method => $value) {
 			\lithium\storage\Session::write($method, $value);
 		}
 	}
 
-    public function show($placeId = null) {
-        if (empty($placeId)) {
-            $this->redirect('/');
-        }
+	public function show($placeId = null) {
+		if (empty($placeId)) {
+			$this->redirect('/');
+		}
 
-        $api = new \app\models\ApontadorApi();
-        $place = $api->getPlace(array('placeid' => $placeId));
+		$api = new \app\models\ApontadorApi();
+		$place = $api->getPlace(array('placeid' => $placeId));
 
-        if ($place) {
-            switch ($place->place->average_rating) {
-                case 1:
-                    $place->place->average_rating = "Péssimo";
-                    break;
-                case 2:
-                    $place->place->average_rating = "Ruim";
-                    break;
-                case 3:
-                    $place->place->average_rating = "Regular";
-                    break;
-                case 4:
-                    $place->place->average_rating = "Bom";
-                    break;
-                case 5:
-                    $place->place->average_rating = "Excelente";
-                    break;
-            }
-            return compact('place');
-        } else {
-            $this->redirect('/');
-        }
-    }
+		if ($place) {
+			switch ($place->place->average_rating) {
+				case 1:
+					$place->place->average_rating = "Péssimo";
+					break;
+				case 2:
+					$place->place->average_rating = "Ruim";
+					break;
+				case 3:
+					$place->place->average_rating = "Regular";
+					break;
+				case 4:
+					$place->place->average_rating = "Bom";
+					break;
+				case 5:
+					$place->place->average_rating = "Excelente";
+					break;
+			}
+			return compact('place');
+		} else {
+			$this->redirect('/');
+		}
+	}
+
 }
--- app/models/ApontadorApi.php
+++ app/models/ApontadorApi.php
@@ -27,23 +27,26 @@ class ApontadorApi {
 	}
 
 	public function getCategories($param=array()) {
-		return $this->request('categories', array(
-			'term' => isset($param['term']) ? $this->removeAccents($param['term']) : '',
-		));
+		$response = $this->request('categories', array(
+					'term'			=> isset($param['term']) ? $this->removeAccents($param['term']) : '',
+				));
+		return json_decode($response, false);
 	}
 
 	public function getCategoriesTop() {
-		return $this->request('categories/top');
+		$response = $this->request('categories/top');
+		return json_decode($response, false);
 	}
 
 	public function getSubcategories($param=array()) {
 		if (empty($param['categoryid'])) {
 			return false;
 		}
-		return $this->request('categories/'.$param['categoryid'].'/subcategories', array(
-			'categoryid'	=> $param['categoryid'],
-			'term'			=> isset($param['term']) ? $this->removeAccents($param['term']) : '',
-		));
+		$response = $this->request('categories/' . $param['categoryid'] . '/subcategories', array(
+					'categoryid'	=> $param['categoryid'],
+					'term'			=> isset($param['term']) ? $this->removeAccents($param['term']) : '',
+				));
+		return json_decode($response, false);
 	}
 
 	public function searchByPoint($param=array()) {
@@ -51,35 +54,37 @@ class ApontadorApi {
 			return false;
 		}
 		$response = $this->request('search/places/bypoint', array(
-			'lat'			=> $param['lat'],
-			'lng'			=> $param['lng'],
-			'radius_mt'		=> isset($param['radius_mt'])	? $param['radius_mt'] : '',
-			'term'			=> isset($param['term']) ? $this->removeAccents($param['term']) : '',
-			'category_id'	=> isset($param['category_id']) ? $param['category_id'] : '',
-			'sort_by'		=> isset($param['sort_by'])		? $param['sort_by'] : '',
-			'order'			=> isset($param['order'])		? $param['order'] : '',
-			'rating'		=> isset($param['rating'])		? $param['rating'] : '',
-			'page'			=> isset($param['page'])		? $param['page'] : '',
-			'limit'			=> isset($param['limit'])		? $param['limit'] : '',
-			'user_id'		=> isset($param['user_id'])		? $param['user_id'] : '',
-		));
+					'lat'			=> $param['lat'],
+					'lng'			=> $param['lng'],
+					'radius_mt'		=> isset($param['radius_mt']) ? $param['radius_mt'] : '',
+					'term'			=> isset($param['term']) ? $this->removeAccents($param['term']) : '',
+					'category_id'	=> isset($param['category_id']) ? $param['category_id'] : '',
+					'sort_by'		=> isset($param['sort_by']) ? $param['sort_by'] : '',
+					'order'			=> isset($param['order']) ? $param['order'] : '',
+					'rating'		=> isset($param['rating']) ? $param['rating'] : '',
+					'page'			=> isset($param['page']) ? $param['page'] : '',
+					'limit'			=> isset($param['limit']) ? $param['limit'] : '',
+					'user_id'		=> isset($param['user_id']) ? $param['user_id'] : '',
+				));
 
 		return json_decode($response, false);
 	}
 
-	public function searchRecursive($param, $type = 'byPoint') {
+	public function searchRecursive($param, $type = 'searchByPoint') {
 		$numFound = 0;
 		$radiusLimit = 10000000;
 		$param['limit'] = !empty($param['limit']) ? $param['limit'] : 20;
-		$param['radius_mt'] = !empty($param['radius_mt'])	 ? $param['radius_mt'] : 10;
-		
+		$param['radius_mt'] = !empty($param['radius_mt']) ? $param['radius_mt'] : 10;
+
 		do {
 			$param['radius_mt'] . " ";
-			$result = $this->searchByPoint($param);
+			$result = $this->$type($param);
 			$numFound = $result->search->result_count;
+			$radius = $param['radius_mt'];
 			$param['radius_mt'] = $param['radius_mt'] * 10;
 		} while ($numFound < $param['limit'] || $param['radius_mt'] > $radiusLimit);
 
+		echo 'radius:' . $radius;
 		return $result;
 	}
 
@@ -88,16 +93,16 @@ class ApontadorApi {
 			return false;
 		}
 		$response = $this->request('search/places/byaddress', array(
-			'country'		=> $param['country'],
-			'state'			=> $param['state'],
-			'city'			=> $this->removeAccents($param['city']),
-			'street'		=> isset($param['street'])		? $param['street'] : '',
-			'number'		=> isset($param['number'])		? $param['number'] : '',
-			'district'		=> isset($param['district'])	? $param['district'] : '',
-			'radius_mt'		=> isset($param['radius_mt'])	? $param['radius_mt'] : '',
-			'term'			=> isset($param['term'])		? $param['term'] : '',
-			'category_id'	=> isset($param['category_id']) ? $param['category_id'] : '',
-		));
+					'country'		=> $param['country'],
+					'state'			=> $param['state'],
+					'city'			=> $this->removeAccents($param['city']),
+					'street'		=> isset($param['street']) ? $param['street'] : '',
+					'number'		=> isset($param['number']) ? $param['number'] : '',
+					'district'		=> isset($param['district']) ? $param['district'] : '',
+					'radius_mt'		=> isset($param['radius_mt']) ? $param['radius_mt'] : '',
+					'term'			=> isset($param['term']) ? $param['term'] : '',
+					'category_id'	=> isset($param['category_id']) ? $param['category_id'] : '',
+				));
 
 		return json_decode($response, false);
 	}
@@ -106,19 +111,19 @@ class ApontadorApi {
 		if (empty($param['zipcode'])) {
 			return false;
 		}
-		
+
 		$response = $this->request('search/places/byzipcode', array(
-			'zipcode'		=> $param['zipcode'],
-			'radius_mt'		=> isset($param['radius_mt'])	? $param['radius_mt'] : '',
-			'term'			=> isset($param['term'])		? $this->removeAccents($param['term']) : '',
-			'category_id'	=> isset($param['category_id']) ? $param['category_id'] : '',
-			'sort_by'		=> isset($param['sort_by'])		? $param['sort_by'] : '',
-			'order'			=> isset($param['order'])		? $param['order'] : '',
-			'rating'		=> isset($param['rating'])		? $param['rating'] : '',
-			'page'			=> isset($param['page'])		? $param['page'] : '',
-			'limit'			=> isset($param['limit'])		? $param['limit'] : '',
-			'user_id'		=> isset($param['user_id'])		? $param['user_id'] : '',
-		));
+					'zipcode'		=> $param['zipcode'],
+					'radius_mt'		=> isset($param['radius_mt']) ? $param['radius_mt'] : '',
+					'term'			=> isset($param['term']) ? $this->removeAccents($param['term']) : '',
+					'category_id'	=> isset($param['category_id']) ? $param['category_id'] : '',
+					'sort_by'		=> isset($param['sort_by']) ? $param['sort_by'] : '',
+					'order'			=> isset($param['order']) ? $param['order'] : '',
+					'rating'		=> isset($param['rating']) ? $param['rating'] : '',
+					'page'			=> isset($param['page']) ? $param['page'] : '',
+					'limit'			=> isset($param['limit']) ? $param['limit'] : '',
+					'user_id'		=> isset($param['user_id']) ? $param['user_id'] : '',
+				));
 
 		return json_decode($response, false);
 	}
@@ -132,7 +137,7 @@ class ApontadorApi {
 	}
 
 	private function request($method, $params=array()) {
-		$default = array('type' => 'json');
+		$default = array('type'		=> 'json');
 
 		$queryString = \http_build_query($params + $default);
 
@@ -153,21 +158,21 @@ class ApontadorApi {
 	}
 
 	private function requestNative($method, $params=array()) {
-		$default = array('type' => 'json');
+		$default = array('type'		=> 'json');
 
 		$queryString = \http_build_query($params + $default);
 
 		$url = $this->config['apiUrl'] . $method . '?' . $queryString;
 
 		$config = array(
-			'host' => $url,
-			'port' => $this->config['port'],
-			'auth' => 'Basic',
-			'username' => $this->config['consumerKey'],
-			'password' => $this->config['consumerSecret'],
-			'timeout' => 10,
-			'socket' => 'Curl',
-			'encoding' => 'UTF-8',
+			'host'					=> $url,
+			'port'					=> $this->config['port'],
+			'auth'					=> 'Basic',
+			'username'				=> $this->config['consumerKey'],
+			'password'				=> $this->config['consumerSecret'],
+			'timeout'				=> 10,
+			'socket'				=> 'Curl',
+			'encoding'				=> 'UTF-8',
 		);
 		$request = new \lithium\net\http\Service($config);
 		$response = $request->post($method, $params);
