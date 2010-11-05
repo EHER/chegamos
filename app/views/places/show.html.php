2a9d513e11f36700aacf9051fee15657ad5a85b5
Rodrigo Prestes <rtprestes@gmail.com>
2010-11-05 11:07:41 -0200

    Exibição do preços em postos de combustível.

--- app/views/places/show.html.php
+++ app/views/places/show.html.php
@@ -19,10 +19,10 @@
 	<?php if(!empty($place->place->phone->area)) echo '(' . $place->place->phone->area . ')'; ?>
 	<?php if(!empty($place->place->phone->number)) echo $place->place->phone->number; ?>
 </p>
+<p>Categoria: <?php echo $place->place->category->subcategory->name; ?></p>
 <p><?php echo $place->place->description; ?></p>
 <?php /*
 <p><?php echo round($place->place->thumbs->up / $place->place->thumbs->total * 100); ?>% recomendam</p>
-<p><?php echo $place->place->category->name; ?> > <?php echo $place->place->category->subcategory->name; ?></p>
 <p><?php echo $place->place->click_count; ?> visitas</p>
 <p><?php echo $place->place->review_count; ?> avaliações</p>
 <p><?php echo $place->place->point->lat; ?></p>
@@ -35,6 +35,30 @@
 	</a>
 </p>
 
+<?php 
+	$gasStationData = array(
+		'price_alcohol' => 'Álcool',
+		'price_gasoline' => 'Gasolina',
+		'price_gasoline_aditivada' => 'Gasolina Aditivada',
+		'price_gasoline_podium' => 'Gasolina Pódium',
+		'price_gasoline_premium' => 'Gasolina Premium',
+		'price_gnv' => 'Gás Natural',
+		'price_kerosene' => 'Querosene'
+	);
+?>
+
+<?php if(isset($place->place->extended->gas_station)) { ?>
+<ul data-role="listview" data-inset="true" data-theme="c" data-dividertheme="b">
+	<li data-role="list-divider">Preços de combustível</li>
+	<?php foreach ($place->place->extended->gas_station as $name => $info) { ?>
+		<?php if (strstr($name, 'price_') && $info != 0) { ?>
+			<li>
+				<?php echo $gasStationData[$name] . ': R$ ' . $info; ?>
+			</li>
+		<?php } ?>
+	<?php } ?>
+</ul>
+<?php } ?>
 <ul data-role="listview" data-inset="true" data-theme="c" data-dividertheme="b">
 	<li>
 		<?php echo $this->html->link("Estou aqui", "/places/checkin?placeId=" . $place->place->id); ?>
