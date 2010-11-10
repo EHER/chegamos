c20b4c070ad0f3863e4e338ab2fafcd2d25089ed
Eher <alexandre.eher@lbslocal.com>
2010-11-10 08:17:56 -0200

    Adicionada tag do site verification do Google.

--- app/views/layouts/default.html.php
+++ app/views/layouts/default.html.php
@@ -14,6 +14,7 @@
 	<?php //echo $this->html->style(array('debug', 'lithium')); ?>
 	<?php echo $this->scripts(); ?>
 	<?php echo $this->html->link('Icon', null, array('type' => 'icon')); ?>
+	<meta name="google-site-verification" content="DHlJPavykQLKD9wWywvKhr_t04fToqn-wK4WPdODQcQ" />
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a1/jquery.mobile-1.0a1.min.css" />
 	<script src="http://code.jquery.com/jquery-1.4.3.min.js"></script>
 	<script src="http://code.jquery.com/mobile/1.0a1/jquery.mobile-1.0a1.min.js"></script>
