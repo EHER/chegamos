725d4cf437a117e0bc5be296308fea1ab5af4845
Eher <alexandre.eher@lbslocal.com>
2010-11-10 08:15:02 -0200

    Adicionada a tag do Google Analytics.

--- app/views/layouts/default.html.php
+++ app/views/layouts/default.html.php
@@ -79,3 +79,16 @@
 		location.href = '<?php echo ROOT_URL; ?>' + 'places/checkin?lat=' + lat + '&lng='+lon;
 	}
 </script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-19619039-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
