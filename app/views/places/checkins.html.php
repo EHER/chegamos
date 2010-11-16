d0573a5321751ae48987edeb055a11d42e0db15a
Eher <alexandre@skd.com.br>
2010-11-16 00:46:06 -0200

    Correção dos links do profile. Criação de links para o profile na listagem de checkins feitos em um local e nos detalhes da avaliação.

--- app/views/places/checkins.html.php
+++ app/views/places/checkins.html.php
@@ -19,7 +19,7 @@
 		<li tabindex="0" class="ui-li-has-thumb ui-li ui-btn ui-btn-up-c" data-theme="c">
 			<div class="ui-btn-text">
 				<img width="64" height="64" src="<?php echo ($visitor->visitor->user->photo ? $visitor->visitor->user->photo : 'http://www.apontador.com.br/apontador_v8/images/accounts/user64.gif'); ?>" class="ui-li-thumb">
-				<h3 class="ui-li-heading"><a href="<?php echo 'http://www.apontador.com.br/profile/' . $visitor->visitor->user->id . '.html'; ?>" class="ui-link-inherit"><?php echo $visitor->visitor->user->name; ?></a></h3>
+				<h3 class="ui-li-heading"><a href="<?php echo ROOT_URL . 'profile/show/' . $visitor->visitor->user->id; ?>" class="ui-link-inherit"><?php echo $visitor->visitor->user->name; ?></a></h3>
 				<p class="ui-li-desc">(<?php echo $visitor->visitor->visits . ($visitor->visitor->visits == 1 ? ' visita' : ' visitas'); ?>)
 				em <?php echo date("d/m H:i", strtotime($visitor->visitor->last_visit)); ?></p>
 			</div>
--- app/views/profile/show.html.php
+++ app/views/profile/show.html.php
@@ -2,7 +2,7 @@
 <?php use \app\models\UserStats; ?>
 
 <h2 style="margin:0;">
-	<?php echo $this->html->link($user->getName(), "/places/show/" . $user->getId()); ?>
+	<?php echo $this->html->link($user->getName(), "/profile/show/" . $user->getId()); ?>
 </h2>
 <?php if ($user->getPhotoUrl()) { ?>
 	<img src="<?php echo $user->getPhotoUrl(); ?>" />
@@ -13,12 +13,12 @@
 
 <ul data-role="listview" data-inset="true" data-theme="c" data-dividertheme="b">
 	<li>
-		<?php echo $this->html->link("Avaliações (" . $user->getStats()->getReviews() . ")", "/profile/" . $user->getId() . "reviews"); ?>
+		<?php echo $this->html->link("Avaliações (" . $user->getStats()->getReviews() . ")", "/profile/" . $user->getId() . "/reviews"); ?>
 	</li>
 	<li>
-		<?php echo $this->html->link("Locais Cadastrados (" . $user->getStats()->getPlaces() . ")", "/profile/" . $user->getId() . "places"); ?>
+		<?php echo $this->html->link("Locais Cadastrados (" . $user->getStats()->getPlaces() . ")", "/profile/" . $user->getId() . "/places"); ?>
 	</li>
 	<li>
-		<?php echo $this->html->link("Fotos (" . $user->getStats()->getPhotos() . ")", "/profile/" . $user->getId() . "photos"); ?>
+		<?php echo $this->html->link("Fotos (" . $user->getStats()->getPhotos() . ")", "/profile/" . $user->getId() . "/photos"); ?>
 	</li>
 </ul>
