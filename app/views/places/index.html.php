f81b7ccbf490c15a7a06342cf001578f260606f6
Eher <alexandre.eher@lbslocal.com>
2011-01-08 13:37:16 -0200

    Inclusão do layout para o menu pessoas (comentado, por enquanto).

--- app/views/places/index.html.php
+++ app/views/places/index.html.php
@@ -4,3 +4,11 @@
 	<li><?php echo $this->html->link('Locais por Categoria', "/places/categories"); ?></li>
 	<li><?php echo $this->html->link('Locais Próximos', "/places/near"); ?></li>
 </ul>
+<!--// Em desenvolvimento
+<ul data-role="listview" data-inset="true" data-theme="c" data-dividertheme="b">
+	<li data-role="list-divider">Pessoas</li>
+	<li><?php echo $this->html->link('Quem eu sigo', "/places/search", array('rel' => 'external')); ?></li>
+	<li><?php echo $this->html->link('Quem me segue', "/places/categories"); ?></li>
+	<li><?php echo $this->html->link('Pessoas por perto', "/places/near"); ?></li>
+</ul>
+-->
