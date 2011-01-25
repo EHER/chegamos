1599eed9f1f8435ad9f05ef52daf86ddbf241a98
Eher <alexandre@skd.com.br>
2011-01-24 23:43:04 -0200

    Formatação do layout das ofertas.

--- app/views/deals/near.html.php
+++ app/views/deals/near.html.php
@@ -2,22 +2,20 @@
 <?php use app\models\DealList; ?>
 <ul data-role="listview" data-inset="true" data-theme="c" data-dividertheme="b">
 	<li data-role="list-divider">Ofertas por perto</li>
-	<?php //var_dump($dealsList); ?>
 	<?php if ($dealsList instanceof DealList) { ?>
 		<?php foreach ($dealsList->getItems() as $deal) { ?>
-		    <li>
-				<span class="placename">
-					<img width="64" height="64" src="<?php echo ($deal->getImageUrl() ? $deal->getImageUrl() : 'http://www.apontador.com.br/apontador_v8/images/accounts/user64.gif'); ?>" class="ui-li-thumb">
-				</span>
-				<br />
-				<p class="ui-li-desc">
-					<p>
-						<?php echo $this->html->link($deal->getTitle(), "/deals/show/" . $deal->getId() . ""); ?>
-					</p>
+		    <li class="ui-li-has-thumb ui-btn ui-btn-icon-right ui-li ui-btn-down-c ui-btn-up-c">
+				<br/>
+				<p class="ui-li-desc" style="white-space:normal;">
+					<img width="84" height="84" src="<?php echo ($deal->getImageUrl() ? $deal->getImageUrl() : 'http://www.apontador.com.br/apontador_v8/images/accounts/user64.gif'); ?>" class="ui-li-thumb">
+					<?php echo $this->html->link($deal->getTitle(), "/deals/show/" . $deal->getId() . ""); ?>
 				</p>
 			</li>
 		<?php } ?>
 	<?php } else { ?>
-	<li>Não existem ofertas por perto.</li>
+	<li>
+		Não encontrei ofertas por aqui<br/>
+		Tente novamente amanhã
+	</li>
 	<?php } ?>
 </ul>
--- app/views/places/index.html.php
+++ app/views/places/index.html.php
@@ -4,16 +4,16 @@
 	<li><?php echo $this->html->link('Locais Próximos', "/places/near"); ?></li>
 </ul>
 <ul data-role="listview" data-inset="true" data-theme="c" data-dividertheme="b">
-	<li data-role="list-divider">Ofertas</li>
-	<li><?php echo $this->html->link('Ofertas Próximas', "/deals/near"); ?></li>
-</ul>
-<ul data-role="listview" data-inset="true" data-theme="c" data-dividertheme="b">
 	<li data-role="list-divider">Pessoas</li>
 	<li><?php echo $this->html->link('Quem eu sigo', "/profile/following"); ?></li>
 	<li><?php echo $this->html->link('Quem me segue', "/profile/followers"); ?></li>
 	<!-- WIP
 	<li><?php echo $this->html->link('Pessoas por perto', "/profile/near"); ?></li>
 	-->
+</ul>
+<ul data-role="listview" data-inset="true" data-theme="c" data-dividertheme="b">
+	<li data-role="list-divider">Ofertas</li>
+	<li><?php echo $this->html->link('Ofertas Próximas', "/deals/near"); ?></li>
 </ul>
 <!-- WIP
 <ul data-role="listview" data-inset="true" data-theme="c" data-dividertheme="b">
