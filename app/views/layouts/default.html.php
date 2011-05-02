cdad221c854149d87efcfd6800e10d711bcd3e52
Rodrigo Prestes <rodrigo.prestes@lbslocal.com>
2011-05-02 14:58:50 -0300

    corrige o tamanho da caixa com a lupa para caber no mobile

--- app/views/layouts/default.html.php
+++ app/views/layouts/default.html.php
@@ -34,7 +34,7 @@
 			<?php echo $this->html->link('Config.', '/settings', array("rel" => "nofollow","data-icon"=>"gear","class"=>"ui-btn-right", "data-transition"=>"slideup")); ?>
 
 			<form method="GET" action="<?php echo ROOT_URL; ?>places/search" style="text-align: center; width:100%">
-				<input type="text" id="name" name="name" value="<?php echo (isset($_GET['name']) ? $_GET['name'] : '');?>" style="display: inline; width: 70%;">
+				<input type="text" id="name" name="name" value="<?php echo (isset($_GET['name']) ? $_GET['name'] : '');?>" style="display: inline; width: 60%;">
 				<input type="submit" value="Buscar" data-inline="true" style="display: inline;"  data-icon="search">
 			</form>
 		</div>
