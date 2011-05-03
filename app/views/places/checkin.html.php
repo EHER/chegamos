0fe2c04fb4eef5d1709b8be07f1c17f3c2889a86
Eher <alexandre@skd.com.br>
2011-05-03 08:06:17 -0300

    Correção da tela de check-in

--- app/views/places/checkin.html.php
+++ app/views/places/checkin.html.php
@@ -9,8 +9,10 @@
 						<?php if(is_array($providers)) { ?>
 							<fieldset data-role="controlgroup">
 								<?php foreach($providers as $provider => $providerName) { ?>
-										<input name="providers[<?php echo $provider;?>]" id="providers[<?php echo $provider;?>]" type="checkbox" checked="checked">
-										<label for="providers[<?php echo $provider;?>]"><?php echo $providerName;?></label>
+										<input name="providers[<?php echo $provider;?>]" id="providers_<?php echo $provider;?>" type="checkbox" checked="checked">
+										<label for="providers_<?php echo $provider;?>">
+											<?php echo $providerName;?>
+										</label>
 								<?php } ?>
 							</fieldset>
 						<?php } ?>
