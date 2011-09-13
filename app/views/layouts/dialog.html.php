b7eca24d7b1abfdf341c641fb4d3f09815abfee1
Eher <alexandre.eher@apontador.com>
2011-09-13 12:09:54 -0300

    Correção da tela de configurações

--- app/views/layouts/dialog.html.php
+++ app/views/layouts/dialog.html.php
@@ -10,15 +10,13 @@
 <html>
 <head>
 	<?php echo $this->html->charset();?>
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.css" />
 	<title>Chegamos! <?php if(!empty($title)) echo "- " . $title; ?></title>
-	<meta name="google-site-verification" content="DHlJPavykQLKD9wWywvKhr_t04fToqn-wK4WPdODQcQ" />
-	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a2/jquery.mobile-1.0a2.min.css" />
 	<link rel="shortcut icon" href="<?php echo ROOT_URL ?>favicon.ico">
-	<script src="http://code.jquery.com/jquery-1.4.4.min.js"></script>
-	<script src="http://code.jquery.com/mobile/1.0a2/jquery.mobile-1.0a2.min.js"></script>
-	<script>
-		$.mobile.page.prototype.options.backBtnText = "Voltar";
-	</script>
+	<meta property="fb:app_id" content="<?php echo FACEBOOK_AP_ID; ?>"/>
+	<meta property="og:site_name" content="chegamos"/>
+<?php echo (isset($meta)) ? $meta : '' ?>
+	<meta name="google-site-verification" content="nSgmfqNOpud7XKqEtIzxAmHppP-oDqE3PGKwLLOeGss" />
 </head>
 <body>
 	<div data-role="page" data-theme="b" id="jqm-home">
@@ -30,20 +28,9 @@
 		</div>
 		<div data-role="footer">&nbsp</div>
 	</div>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-19798490-1']);
-  _gaq.push(['_setDomainName', 'none']);
-  _gaq.push(['_setAllowLinker', true]);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
+	<script src="http://code.jquery.com/jquery-1.5.2.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.js"></script>
+	<script src="<?php echo ROOT_URL ?>js/jquery.cookie.js"></script>
+	<script src="<?php echo ROOT_URL ?>js/chegamos.js"></script>
 </body>
 </html>
--- app/views/settings/index.html.php
+++ app/views/settings/index.html.php
@@ -1,63 +1,63 @@
 <ul data-role="listview" data-inset="true" data-theme="c" data-dividertheme="b">
-	<li data-role="list-divider">Configurações de Conta</li>
-	<li data-role="list-divider">
-		<?php if ($apontador['logged']) { ?>
-			<img src="<?php echo ROOT_URL;?>img/ok.png" class="ui-li-icon ui-li-thumb">
-			Apontador:
-			<a href="<?php echo ROOT_URL;?>oauth/logout/apontador" rel="external">Desconectar (<?php echo $apontador['name'];?>)</a>
-		<?php } else { ?>
-			<img src="<?php echo ROOT_URL;?>img/nok.png" class="ui-li-icon ui-li-thumb">
-			Apontador:
-			<a href="<?php echo ROOT_URL;?>oauth/authorize/apontador" rel="external">Conectar</a>
-		<?php } ?>
-	</li>
-	<li data-role="list-divider">
+    <li data-role="list-divider">Configurações de Conta</li>
+    <li data-role="list-divider">
+        <?php if ($apontador['logged']) { ?>
+            <img src="<?php echo ROOT_URL; ?>/img/ok.png" class="ui-li-icon ui-li-thumb">
+    			Apontador:
+            <a href="<?php echo ROOT_URL; ?>/oauth/logout/apontador" rel="external">Desconectar (<?php echo $apontador['name']; ?>)</a>
+        <?php } else { ?>
+            <img src="<?php echo ROOT_URL; ?>/img/nok.png" class="ui-li-icon ui-li-thumb">
+    			Apontador:
+            <a href="<?php echo ROOT_URL; ?>/oauth/authorize/apontador" rel="external">Conectar</a>
+        <?php } ?>
+    </li>
+    <li data-role="list-divider">
 
-		<?php if ($foursquare['logged']) { ?>
-			<img src="<?php echo ROOT_URL;?>img/ok.png" class="ui-li-icon ui-li-thumb">
-			Foursquare:
-			<a href="<?php echo ROOT_URL;?>oauth/logout/foursquare" rel="external">Desconectar (<?php echo $foursquare['name'];?>)</a>
-		<?php } else { ?>
-			<img src="<?php echo ROOT_URL;?>img/nok.png" class="ui-li-icon ui-li-thumb">
-			Foursquare:
-			<a href="<?php echo ROOT_URL;?>oauth/authorize/foursquare" rel="external">Conectar</a>
-		<?php } ?>
-	</li>
-	<li data-role="list-divider">
-		<?php if ($twitter['logged']) { ?>
-			<img src="<?php echo ROOT_URL;?>img/ok.png" class="ui-li-icon ui-li-thumb">
-			Twitter:
-			<a href="<?php echo ROOT_URL;?>oauth/logout/twitter" rel="external">Desconectar (@<?php echo $twitter['name'];?>)</a>
-		<?php } else { ?>
-			<img src="<?php echo ROOT_URL;?>img/nok.png" class="ui-li-icon ui-li-thumb">
-			Twitter:
-			<a href="<?php echo ROOT_URL;?>oauth/authorize/twitter" rel="external">Conectar</a>
-		<?php } ?>
-	</li>
-	<li data-role="list-divider">
-		<?php if ($facebook['logged']) { ?>
-			<img src="<?php echo ROOT_URL;?>img/ok.png" class="ui-li-icon ui-li-thumb">
-			Facebook:
-			<a href="<?php echo ROOT_URL;?>oauth/logout/facebook" rel="external">Desconectar (<?php echo $facebook['name'];?>)</a>
-		<?php } else { ?>
-			<img src="<?php echo ROOT_URL;?>img/nok.png" class="ui-li-icon ui-li-thumb">
-			Facebook:
-			<a href="<?php echo ROOT_URL;?>oauth/authorize/facebook" rel="external">Conectar</a>
-		<?php } ?>
-	</li>
-	<li data-role="list-divider">
-		<?php if ($orkut['logged']) { ?>
-			<img src="<?php echo ROOT_URL;?>img/ok.png" class="ui-li-icon ui-li-thumb">
-			Orkut:
-			<a href="<?php echo ROOT_URL;?>oauth/logout/orkut" rel="external">Desconectar (<?php echo $orkut['name'];?>)</a>
-		<?php } else { ?>
-			<img src="<?php echo ROOT_URL;?>img/nok.png" class="ui-li-icon ui-li-thumb">
-			Orkut:
-			<a href="<?php echo ROOT_URL;?>oauth/authorize/orkut" rel="external">Conectar</a>
-		<?php } ?>
-	</li>
+        <?php if ($foursquare['logged']) { ?>
+            <img src="<?php echo ROOT_URL; ?>/img/ok.png" class="ui-li-icon ui-li-thumb">
+    			Foursquare:
+            <a href="<?php echo ROOT_URL; ?>/oauth/logout/foursquare" rel="external">Desconectar (<?php echo $foursquare['name']; ?>)</a>
+        <?php } else { ?>
+            <img src="<?php echo ROOT_URL; ?>/img/nok.png" class="ui-li-icon ui-li-thumb">
+    			Foursquare:
+            <a href="<?php echo ROOT_URL; ?>/oauth/authorize/foursquare" rel="external">Conectar</a>
+        <?php } ?>
+    </li>
+    <li data-role="list-divider">
+        <?php if ($twitter['logged']) { ?>
+            <img src="<?php echo ROOT_URL; ?>/img/ok.png" class="ui-li-icon ui-li-thumb">
+    			Twitter:
+            <a href="<?php echo ROOT_URL; ?>/oauth/logout/twitter" rel="external">Desconectar (@<?php echo $twitter['name']; ?>)</a>
+        <?php } else { ?>
+            <img src="<?php echo ROOT_URL; ?>/img/nok.png" class="ui-li-icon ui-li-thumb">
+    			Twitter:
+            <a href="<?php echo ROOT_URL; ?>/oauth/authorize/twitter" rel="external">Conectar</a>
+        <?php } ?>
+    </li>
+    <li data-role="list-divider">
+        <?php if ($facebook['logged']) { ?>
+            <img src="<?php echo ROOT_URL; ?>/img/ok.png" class="ui-li-icon ui-li-thumb">
+    			Facebook:
+            <a href="<?php echo ROOT_URL; ?>/oauth/logout/facebook" rel="external">Desconectar (<?php echo $facebook['name']; ?>)</a>
+        <?php } else { ?>
+            <img src="<?php echo ROOT_URL; ?>/img/nok.png" class="ui-li-icon ui-li-thumb">
+    			Facebook:
+            <a href="<?php echo ROOT_URL; ?>/oauth/authorize/facebook" rel="external">Conectar</a>
+        <?php } ?>
+    </li>
+    <li data-role="list-divider">
+        <?php if ($orkut['logged']) { ?>
+            <img src="<?php echo ROOT_URL; ?>/img/ok.png" class="ui-li-icon ui-li-thumb">
+    			Orkut:
+            <a href="<?php echo ROOT_URL; ?>/oauth/logout/orkut" rel="external">Desconectar (<?php echo $orkut['name']; ?>)</a>
+        <?php } else { ?>
+            <img src="<?php echo ROOT_URL; ?>/img/nok.png" class="ui-li-icon ui-li-thumb">
+    			Orkut:
+            <a href="<?php echo ROOT_URL; ?>/oauth/authorize/orkut" rel="external">Conectar</a>
+        <?php } ?>
+    </li>
 </ul>
-<form method="GET" action="<?php echo ROOT_URL;?>">
-	<button type="submit">Salvar Configurações</button>
+<form method="GET" action="<?php echo ROOT_URL; ?>">
+    <button type="submit">Salvar Configurações</button>
 </form>
 
