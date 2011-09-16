145e3b130b387d8d74c7a32804ddbfc9ebd819ab
Eher <alexandre@skd.com.br>
2011-09-15 21:40:15 -0300

    Organização dos links de futuras features na home

--- app/views/places/index.html.php
+++ app/views/places/index.html.php
@@ -9,19 +9,21 @@
 	<li><?php echo $this->html->link('Meu Perfil', "/profile/show", array("rel"=>"external")); ?></li>
 	<li><?php echo $this->html->link('Amigos', "/profile/following", array("rel"=>"external")); ?></li>
 	<li><?php echo $this->html->link('Seguidores', "/profile/followers", array("rel"=>"external")); ?></li>
-	<!-- WIP
+<?php /* WIP
 	<li><?php echo $this->html->link('Pessoas por perto', "/profile/near"); ?></li>
-	-->
+ */?>
 </ul>
 <ul data-role="listview" data-inset="true" data-theme="c" data-dividertheme="b">
 	<li data-role="list-divider">Informações</li>
 	<li><?php echo $this->html->link('Ofertas por perto', "/deals/near"); ?></li>
+<?php /* WIP
+	<li><?php echo $this->html->link('Sobre o Chegamos!', "/about"); ?></li>
+ */?>
 </ul>
-<!-- WIP
+<?php /* WIP
 <ul data-role="listview" data-inset="true" data-theme="c" data-dividertheme="b">
 	<li data-role="list-divider">Todo</li>
 	<li><?php echo $this->html->link('Fazer Avaliações', "/todo/reviews"); ?></li>
 	<li><?php echo $this->html->link('Enviar Fotos', "/todo/photos"); ?></li>
 </ul>
--->
-
+ */?>
