4ad0ef48363ccf876619b396491f5a7ff8f5abc1
Eher <alexandre@skd.com.br>
2011-09-19 23:35:15 -0300

    Versão light do Chegamos! o.O

--- app/views/layouts/default.html.php
+++ app/views/layouts/default.html.php
@@ -13,9 +13,11 @@
     <title>Chegamos! <?php if(!empty($title)) echo "- " . $title; ?></title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php if(!defined('LIGHT_VERSION') || LIGHT_VERSION === false) { ?>	
     <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0b3/jquery.mobile-1.0b3.min.css" />
     <script src="http://code.jquery.com/jquery-1.6.2.min.js"></script>
     <script src="http://code.jquery.com/mobile/1.0b3/jquery.mobile-1.0b3.min.js"></script>
+<?php } ?>
 	<link rel="shortcut icon" href="<?php echo ROOT_URL ?>favicon.ico">
 	<meta property="fb:app_id" content="<?php echo FACEBOOK_AP_ID; ?>"/>
 	<meta property="og:site_name" content="chegamos"/>
@@ -56,8 +58,10 @@
 				</a>
 		</div>
 	</div>
+<?php if(!defined('LIGHT_VERSION') || LIGHT_VERSION === false) { ?>	
     <input type="hidden" id="rootUrl" value="<?php echo ROOT_URL;?>"/>
 	<script src="<?php echo ROOT_URL ?>js/jquery.cookie.js"></script>
 	<script src="<?php echo ROOT_URL ?>js/chegamos.js"></script>
+<?php } ?>
 </body>
 </html>
