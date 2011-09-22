a2f48d5bd3f49989797d80afe2337b3e3562ae98
Eher <alexandre.eher@apontador.com>
2011-09-22 10:19:15 -0300

    Alteração da home para ficar mais simples

--- app/views/places/index.html.php
+++ app/views/places/index.html.php
@@ -8,8 +8,6 @@
 	<li data-role="list-divider">Pessoas</li>
 	<li><?php echo $this->html->link('Meu Perfil', "/profile/show", array("rel"=>"external")); ?></li>
 	<li><?php echo $this->html->link('Pessoas por perto', "/profile/near"); ?></li>
-	<li><?php echo $this->html->link('Amigos', "/profile/following", array("rel"=>"external")); ?></li>
-	<li><?php echo $this->html->link('Seguidores', "/profile/followers", array("rel"=>"external")); ?></li>
 </ul>
 <ul data-role="listview" data-inset="true" data-theme="c" data-dividertheme="b">
 	<li data-role="list-divider">Informações</li>
