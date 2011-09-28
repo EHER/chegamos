918480dd0d7abb76d761d5e188b9ee5eb7a6532f
Eher <alexandre.eher@apontador.com>
2011-09-28 09:03:53 -0300

    Organização do exemplo do config.php

--- app/config/config.exemplo.php
+++ app/config/config.exemplo.php
@@ -24,29 +24,70 @@ ini_set("default_charset", "utf-8");
  * Qualquer dúvida sobre a api do apontador, a documentação está em
  * http://api.apontador.com.br/pt/acesso.html
  */
-define('ROOT_URL', 'http://localhost/chegamos/');
-define('STATIC_URL', 'http://localhost/chegamos/');
-define('LIGHT_VERSION', false);
-define('APONTADOR_URL', 'api.apontador.com.br/v1/');
-define('APONTADOR_PORT', 80);
 define('APONTADOR_CONSUMER_KEY', '');
 define('APONTADOR_CONSUMER_SECRET', '');
-define('APONTADOR_TIMEOUT', 10);
-define('APONTADOR_POST_LOGIN', false);
 
-define('THEME_MAIN', 'b');
-define('THEME_LIST', 'c');
+/**
+ * Configuração das URLs
+ */
+define('ROOT_URL', 'http://localhost/chegamos/');
+define('STATIC_URL', 'http://localhost/chegamos/');
 
+/**
+ * Habilita a versão sem javascript/css
+ */
+define('LIGHT_VERSION', false);
+
+/**
+ * Chaves para fazer connect do Foursquare
+ */
 define('FOURSQUARE_CONSUMER_KEY', '');
 define('FOURSQUARE_CONSUMER_SECRET', '');
 
+/**
+ * Chaves para fazer connect do Twitter
+ */
 define('TWITTER_CONSUMER_KEY', '');
 define('TWITTER_CONSUMER_SECRET', '');
 
+/**
+ * Dados para fazer connect com Facebook
+ */
 define('FACEBOOK_AP_ID', '');
 define('FACEBOOK_SECRET', '');
 
+/**
+ * Chaves para fazer connect com Orkut
+ */
 define('ORKUT_CONSUMER_KEY', '');
 define('ORKUT_CONSUMER_SECRET', '');
 
+/**
+ * Chave para usar API do Google
+ */
 define('GOOGLE_APIS_KEY', '');
+
+/**
+ * Configuração da Api Apontador
+ */
+define('APONTADOR_URL', 'api.apontador.com.br/v1/');
+define('APONTADOR_PORT', 80);
+define('APONTADOR_TIMEOUT', 10);
+
+/**
+ * Habilita e configura o sevidor do Memcached
+ */
+define('USE_MEMCACHED', false);
+define('MEMCACHED_SERVER', '127.0.0.1:11211');
+
+/**
+ * Contiguração de tema
+ */
+define('THEME_MAIN', 'b');
+define('THEME_LIST', 'c');
+
+/**
+ * Faz login via POST no lugar de Oauth
+ * @deprecated
+ */
+define('APONTADOR_POST_LOGIN', false);
