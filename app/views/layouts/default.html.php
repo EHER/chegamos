c0b777ba7d4276cb7cadeb40fd51e3c50082d28d
Alexandre Eher <alexandre@skd.com.br>
2012-06-26 09:54:23 -0300

    Prevents parsing from blocking the initial page load by deferring it until the browser's UI thread is not busy doing something else.

--- app/views/layouts/default.html.php
+++ app/views/layouts/default.html.php
@@ -29,7 +29,7 @@ if(!empty($abmType)) {
 	<link rel="shortcut icon" href="<?php echo STATIC_URL ?>favicon.ico"/>
 <?php if(!defined('LIGHT_VERSION') || LIGHT_VERSION === false) { ?>	
     <link rel="stylesheet" href="<?php echo STATIC_URL ?>min/?g=css"/>
-    <script src="<?php echo STATIC_URL ?>min/?g=js_head"></script>
+    <script async src="<?php echo STATIC_URL ?>min/?g=js_head"></script>
 <?php } ?>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
@@ -66,7 +66,7 @@ if(!empty($abmType)) {
 	</div>
     <input type="hidden" id="rootUrl" value="<?php echo ROOT_URL;?>"/>
 <?php if(!defined('LIGHT_VERSION') || LIGHT_VERSION === false) { ?>	    
-    <script src="<?php echo STATIC_URL ?>min/?g=js_body"></script>
+    <script async src="<?php echo STATIC_URL ?>min/?g=js_body"></script>
 <?php } ?>
 </body>
 </html>
